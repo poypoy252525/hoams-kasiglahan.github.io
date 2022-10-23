4fef2ead1722ca785e6cfbfeb1c2f36129929a16
poypoy252525 <61503297+poypoy252525@users.noreply.github.com>
2022-10-23 13:12:55 +0800

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const items = document.querySelectorAll(".collapsible");
 items.forEach(item => {
     item.addEventListener("click", function() {
-        item.classList.toggle("collapsible__expanded");
+        this.classList.toggle("collapsible__expanded");
     });
 });
